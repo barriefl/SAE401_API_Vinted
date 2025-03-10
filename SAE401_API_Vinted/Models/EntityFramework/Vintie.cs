33d9a74dab30ad92c0947db1cb4a36d3f00de39e
Enzo Villaret <Enzo.Villaret@etu.univ-smb.fr>
2025-03-10 15:44:40 +0100

    Début model Vintie

---
+++ SAE401_API_Vinted/Models/EntityFramework/Vintie.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SAE401_API_Vinted.Models.EntityFramework
+{
+    [Table("t_e_vinties_vnt")]
+    public class Vintie
+    {
+        [Key]
+        [Column("vnt_id")]
+        public int VintieId { get; set; }
+
+        [Column("vnt_pseudo")]
+        [StringLength(50)]
+        public string Pseudo { get; set; }
+
+        [Column("tco_codetypecompte")]
+        public int CodeTypeCompte { get; set; }
+
+        [Column("vnt_nom")]
+        public string Nom { get; set; }
+
+        [Column("vnt_prenom")]
+        public string Prenom { get; set; }
+
+        [Column("vnt_civilite")]
+        public string Civilite { get; set; }
+
+        [Column("vnt_mail")]
+        [EmailAddress(ErrorMessage = "Format de mail incorrect")]
+        [StringLength(150, MinimumLength = 6, ErrorMessage = "La longueru d'un mail se situe entre 6 et 150 caractères.")]
+        public string Mail { get; set; }
+
+        [Column("vnt_pwd")]
+        [PasswordPropertyText]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un carac spécial (#?!@$%^&*-)")]
+        public string Pwd { get; set; }
+
+        [Column("vnt_telephone")]
+        [RegularExpression(@"[0-9]{10}$", ErrorMessage = "Le numéro de tel doit contenir 10 chiffres")]
+        public string Telephone { get; set; }
+
+        [Column("vnt_datenaissance")]
+        public DateTime DateNaissance { get; set; }
+
+        [Column("vnt_urlphoto")]
+        public string URLPhoto { get; set; }
+
+        [Column("vnt_dateinscription")]
+        public DateTime DateInscription { get; set; }
+
+        [Column("vnt_montantcompte")]
+        public double MontantCompte { get; set; }
+
+        [Column("vnt_statut")]
+        public string Statut { get; set; }
+
+        [Column("vnt_consentement")]
+        public bool Consentement { get; set; }
+
+        [Column("vnt_siret")]
+        public string Siret { get; set; }
+
+        [ForeignKey(nameof(FilmId))]
+        [InverseProperty(nameof(Film.NotesFilm))]
+        public virtual Film FilmIdNavigation { get; set; } = null!;
+    }
+}
