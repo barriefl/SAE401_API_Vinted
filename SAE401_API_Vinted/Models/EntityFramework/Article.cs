8ac107c8c210bf09061e5ef200378930ab7b2079
YannisUSMB <yannis.mauline@etu.univ-smb.fr>
2025-03-12 10:20:46 +0100

    Résolution conflit

--- SAE401_API_Vinted/Models/EntityFramework/Article.cs
+++ SAE401_API_Vinted/Models/EntityFramework/Article.cs
@@ -46,9 +46,6 @@ namespace SAE401_API_Vinted.Models.EntityFramework
         [Column("art_compteurlike")]
         public int CompteurLike { get; set; }
 
-=======
-=======
-=======
         [InverseProperty(nameof(EtatArticle.EtatsDesArticles))]
         public virtual EtatArticle EtatDeArticle { get; set; } = null!;
 
@@ -59,12 +56,8 @@ namespace SAE401_API_Vinted.Models.EntityFramework
         public virtual ICollection<Conversation> ListeConversation { get; set; } = new List<Conversation>();
         [InverseProperty(nameof(Marque.MarquesDesArticles))]
         public virtual Marque MarqueDeArticle { get; set; } = null!;
-<<<<<<< HEAD
 
         [InverseProperty(nameof(Image.ArticleDeImage))]
         public virtual ICollection<Image> ImagesDeArticle { get; set; } = new List<Image>();
-=======
->>>>>>> e9db82f8623cd204e10f4c8d011153cd92dff540
->>>>>>> bfc90972f10343e31a8229142006c98ab004dd14
     }
 }
