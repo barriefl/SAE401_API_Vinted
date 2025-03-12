27746e6d33cb01aad83ce92b43a5583ac2f22592
mauliney <yannis.mauline@etu.univ-smb.fr>
2025-03-12 14:50:28 +0100

    Modification tables Matiere et Article + Ajout tables : Couleur, CouleurArticle

---
+++ SAE401_API_Vinted/Models/EntityFramework/CouleurArticle.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SAE401_API_Vinted.Models.EntityFramework
+{
+    [Table("t_j_couleurarticle_cla")]
+    public class CouleurArticle
+    {
+        [Key]
+        [Column("cla_articleid")]
+        [ForeignKey(nameof(Article.ArticleId))]
+        public int ArticleId { get; set; }
+
+        [Key]
+        [Column("cla_couleirId")]
+        [ForeignKey(nameof(Couleur.CouleurId))]
+        public int CouleurId { get; set; }
+
+        [InverseProperty(nameof(Article.))]
+        public virtual Article ArticleConcerne { get; set; } = null!;
+
+        [InverseProperty(nameof(Couleur.))]
+        public virtual Couleur CouleurConcernee { get; set; } = null!;
+    }
+}
