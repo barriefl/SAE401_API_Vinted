905b5a603d13b3531ebf3355905c75efc6630161
mauliney <yannis.mauline@etu.univ-smb.fr>
2025-03-12 17:17:46 +0100

    Modification ForeignKey Pays, Taille, TailleArticle, Transaction, TypeAdresse, TypeAvis, TypeTaille, Ville

--- SAE401_API_Vinted/Models/EntityFramework/Ville.cs
+++ SAE401_API_Vinted/Models/EntityFramework/Ville.cs
@@ -9,33 +9,32 @@ namespace SAE401_API_Vinted.Models.EntityFramework
     {
         [Key]
         [Column("vil_id")]
-        public int VilleID { get; set; }
+        public int VilleId { get; set; }
 
-        [Column("vil_codepays")]
-        [ForeignKey(nameof(Pays.PaysID))]
-        public int CodePays { get; set; }
+        [Required]
+        [Column("pay_id")]
+        public int PaysId { get; set; }
 
         [Required]
         [Column("vil_nom")]
         public string Nom { get; set; } = null!;
 
         [Required]
-        [Column("vil_codepostal")]
+        [Column("vil_codepostal", TypeName = "char(5)")]
         [RegularExpression(@"^\d{5}$", ErrorMessage = "Le code postal doit être composé de 5 chiffres.")]
         public int CodePostal { get; set; }
 
         [Column("vil_latitude")]
-        public float Latitude { get; set; }
+        public float? Latitude { get; set; }
 
         [Column("vil_longitude")]
-        public float Longitude { get; set; }
+        public float? Longitude { get; set; }
 
-        [InverseProperty(nameof(Adresse.VilleAdresse))]
-        public virtual ICollection<Adresse> AdressesVilles { get; set; } = new List<Adresse>();
-
+        [ForeignKey(nameof(PaysId))]
         [InverseProperty(nameof(Pays.VillesPays))]
         public virtual Pays PaysVille { get; set; } = null!;
 
-
+        [InverseProperty(nameof(Adresse.VilleAdresse))]
+        public virtual ICollection<Adresse> AdressesVilles { get; set; } = new List<Adresse>();
     }
 }
