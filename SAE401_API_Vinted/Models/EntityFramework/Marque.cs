001d769ac2301daf5bdf1b6ab90272787ed00beb
YannisUSMB <yannis.mauline@etu.univ-smb.fr>
2025-03-12 09:02:21 +0100

    Ajout tables sans InverseProperty : Article, Marque, Retour, StatusRetour, TypeRetour

---
+++ SAE401_API_Vinted/Models/EntityFramework/Marque.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SAE401_API_Vinted.Models.EntityFramework
+{
+    [Table("t_e_marque_mrq")]
+    public class Marque
+    {
+        [Key]
+        [Column("mrq_id")]
+        public int MarqueId { get; set; }
+
+        [Required]
+        [Column("mrq_libelle")]
+        [StringLength(150)]
+        public string Libelle { get; set; } = null!;
+    }
+}
---
+++ SAE401_API_Vinted/Models/EntityFramework/TypeRetour.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SAE401_API_Vinted.Models.EntityFramework
+{
+    [Table("t_e_typeretour_tpr")]
+    public class TypeRetour
+    {
+        [Key]
+        [Column("tpr_id")]
+        public int TypeRetourId { get; set; }
+
+        [Required]
+        [Column("tpr_libelle")]
+        [StringLength(50)]
+        public string Libelle { get; set; } = null!;
+
+
+    }
+}
