b8a8a91ede295fff1fe5d3fea7d2d9e948e8d3db
Anthony Marques Felix <Anthony.Marques-Felix@etu.univ-smb.fr>
2025-03-12 15:50:41 +0100

    DbContext

--- SAE401_API_Vinted/Models/EntityFramework/VintedDBContext.cs
+++ SAE401_API_Vinted/Models/EntityFramework/VintedDBContext.cs
@@ -15,9 +15,13 @@ namespace SAE401_API_Vinted.Models.EntityFramework
 
         public virtual DbSet<Article> Articles { get; set; }
 
+        public virtual DbSet<ArticleTaille> ArticlesTailles { get; set; }
+
         public virtual DbSet<Avis> Avis { get; set; }
 
         public virtual DbSet<CarteBancaire> CartesBancaires { get; set; }
+
+        public virtual DbSet<Commande> Commandes { get; set; }
 
         public virtual DbSet<CompteBancaire> ComptesBancaires { get; set; }
 
@@ -25,21 +29,50 @@ namespace SAE401_API_Vinted.Models.EntityFramework
 
         public virtual DbSet<EtatArticle> EtatsArticles { get; set; }
 
+        public virtual DbSet<Expediteur> Expediteurs { get; set; }
+
+        public virtual DbSet<Favoris> Favoris { get; set; }
+
+        public virtual DbSet<FormatColis> FormatColis { get; set; }
+
+        public virtual DbSet<Horaire> Horaires { get; set; }
+
+        public virtual DbSet<Image> Images { get; set; }
+
+        public virtual DbSet<Jour> Jours { get; set; }
+
         public virtual DbSet<Marque> Marques { get; set; }
 
+        public virtual DbSet<Matiere> Matieres { get; set; }
+     
         public virtual DbSet<Message> Messages { get; set; }
+
+        public virtual DbSet<MotifSignalement> MotifsSignalements { get; set; }
 
         public virtual DbSet<Offre> Offres { get; set; }
 
         public virtual DbSet<Pays> Pays { get; set; }
 
+        public virtual DbSet<PointRelais> PointsRelais { get; set; }
+
+        public virtual DbSet<PointRelaisFavoris> PointsRelaisFavoris { get; set; }
+
         public virtual DbSet<Possede> Possede { get; set; }
+
+        public virtual DbSet<Preference> Preferences { get; set; }
 
         public virtual DbSet<Reside> Reside { get; set; }
 
         public virtual DbSet<Retour> Retours { get; set; }
 
+        public virtual DbSet<Signalement> Signalements { get; set; }
+
         public virtual DbSet<StatusRetour> StatusRetours { get; set; }
+        public virtual DbSet<StatusSignalement> StatusSignalements { get; set; }
+
+        public virtual DbSet<Taille> Tailles { get; set; }
+
+        public virtual DbSet<Transaction> Transactions { get; set; }
 
         public virtual DbSet<TypeAdresse> TypesAdresses { get; set; }
 
@@ -51,13 +84,15 @@ namespace SAE401_API_Vinted.Models.EntityFramework
 
         public virtual DbSet<TypeStatusOffre> TypesStatusOffres { get; set; }
 
+        public virtual DbSet<TypeTaille> TypeTailles { get; set; }
+                                                                                                                 
         public virtual DbSet<Ville> Villes { get; set; }
 
         public virtual DbSet<Vintie> Vinties { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Database=SAE401_API_Vinted;Username=postgres;Password=postgres");
+            optionsBuilder.UseNpgsql("Host=localhost;Database=SAE401_API_Vinted;Username=postgres;Password=");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -99,6 +134,34 @@ namespace SAE401_API_Vinted.Models.EntityFramework
             {
 
                 entity.Property(e => e.DateInscription).HasDefaultValueSql("now()");
+
+            });
+
+            modelBuilder.Entity<Message>(entity =>
+            {
+
+                entity.Property(e => e.MessageDateEnvoi).HasDefaultValueSql("now()");
+
+            });
+
+            modelBuilder.Entity<Transaction>(entity =>
+            {
+
+                entity.Property(e => e.DateTransaction).HasDefaultValueSql("now()");
+
+            });
+
+            modelBuilder.Entity<Article>(entity =>
+            {
+
+                entity.Property(e => e.DateAjout).HasDefaultValueSql("now()");
+
+            });
+
+            modelBuilder.Entity<Signalement>(entity =>
+            {
+
+                entity.Property(e => e.DateOuvertureTicket).HasDefaultValueSql("now()");
 
             });
 
