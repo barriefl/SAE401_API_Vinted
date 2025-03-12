41ea32c33a6b5739f6d3b38e5cca21c6ec765ef7
villaree <villaree@D253-S11>
2025-03-12 11:21:46 +0100

    Modification format pk Appartient + Modification Caracteristiques taille

--- SAE401_API_Vinted/Models/EntityFramework/Appartient.cs
+++ SAE401_API_Vinted/Models/EntityFramework/Appartient.cs
@@ -1,24 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace SAE401_API_Vinted.Models.EntityFramework
 {
     [Table("t_j_appartient_app")]
+    [PrimaryKey(nameof(CompteId), nameof(VintieId))]
     public class Appartient
     {
         [Key]
-        [Column("cob_id")]
+        [Column("app_comptebancaireid")]
+        [ForeignKey(nameof(CompteBancaire.CompteId))]
         public int CompteId { get; set; }
 
         [Key]
-        [Column("vnt_id")]
+        [Column("app_vintieid")]
+        [ForeignKey(nameof(Vintie.VintieId))]
         public int VintieId { get; set; }
 
-        [ForeignKey(nameof(CompteId))]
         [InverseProperty(nameof(CompteBancaire.AppartientCompte))]
         public virtual CompteBancaire CompteIdNavigation { get; set; } = null!;
 
-        [ForeignKey(nameof(VintieId))]
         [InverseProperty(nameof(Vintie.AppartienentVintie))]
         public virtual Vintie VintieIdNavigation { get; set; } = null!;
     }
