35971593f020fb0d2cdd404b22aaf32dd3bddb2a
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-03-12 15:16:17 +0100

    Ajout d'une contrainte sur offre.

--- SAE401_API_Vinted/Models/EntityFramework/Offre.cs
+++ SAE401_API_Vinted/Models/EntityFramework/Offre.cs
@@ -6,6 +6,8 @@ namespace SAE401_API_Vinted.Models.EntityFramework
     [Table("t_e_offre_ofr")]
     public class Offre : Message
     {
+        [Required]
+        [Range(0, 9999.99)]
         [Column("ofr_montant", TypeName = "numeric(6,2)")]
         public double OffreMontant { get; set; }
 
