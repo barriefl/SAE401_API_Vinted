f41c7a9912d849202cf92757c7a81db48f74b2db
Anthony Marques Felix <Anthony.Marques-Felix@etu.univ-smb.fr>
2025-03-12 10:12:03 +0100

    DBContext

--- SAE401_API_Vinted/Models/EntityFramework/VintedDBContext.cs
+++ SAE401_API_Vinted/Models/EntityFramework/VintedDBContext.cs
@@ -1,6 +1,55 @@
-﻿namespace SAE401_API_Vinted.Models.EntityFramework
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SAE401_API_Vinted.Models.EntityFramework
 {
-    public class VintedDBContext
+    public partial class VintedDBContext : DbContext
     {
+        public VintedDBContext() {}
+
+        public VintedDBContext(DbContextOptions<VintedDBContext> options) : base(options) { }
+
+        public virtual DbSet<Adresse> Adresses { get; set; }
+
+        public virtual DbSet<Appartient> Appartient { get; set; }
+
+        public virtual DbSet<Article> Articles { get; set; }
+
+        public virtual DbSet<Avis> Avis { get; set; }
+
+        public virtual DbSet<CarteBancaire> CartesBancaires { get; set; }
+
+        public virtual DbSet<CompteBancaire> ComptesBancaires { get; set; }
+
+        public virtual DbSet<Conversation> Conversations { get; set; }
+        
+        public virtual DbSet<Marque> Marques { get; set; }
+
+        public virtual DbSet<Message> Messages { get; set; }
+
+        public virtual DbSet<Offre> Offres { get; set; }
+
+        public virtual DbSet<Pays> Pays { get; set; }
+
+        public virtual DbSet<Possede> Possede { get; set; }
+
+        public virtual DbSet<Reside> Reside { get; set; }
+
+        public virtual DbSet<Retour> Retours { get; set; }
+
+        public virtual DbSet<StatusRetour> StatusRetours { get; set; }
+
+        public virtual DbSet<TypeAdresse> TypesAdresses { get; set; }
+
+        public virtual DbSet<TypeAvis> TypesAvis { get; set; }
+
+        public virtual DbSet<TypeCompte> TypesComptes { get; set; }
+
+        public virtual DbSet<TypeRetour> TypesRetours { get; set; }
+
+        public virtual DbSet<TypeStatusOffre> TypesStatusOffres { get; set; }
+
+        public virtual DbSet<Ville> Villes { get; set; }
+
+        public virtual DbSet<Vintie> Vinties { get; set; }
     }
 }
