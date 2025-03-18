3df5c8ad9f3656c5dfda05d533decd678595d83f
Anthony Marques Felix <Anthony.Marques-Felix@etu.univ-smb.fr>
2025-03-18 10:57:30 +0100

    VintiesController

---
+++ SAE401_API_Vinted/Controllers/VintiesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAE401_API_Vinted.Models.EntityFramework;
+
+namespace SAE401_API_Vinted.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VintiesController : ControllerBase
+    {
+        private readonly VintedDBContext _context;
+
+        public VintiesController(VintedDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Vinties
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Vintie>>> GetVinties()
+        {
+            return await _context.Vinties.ToListAsync();
+        }
+
+        // GET: api/Vinties/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Vintie>> GetVintie(int id)
+        {
+            var vintie = await _context.Vinties.FindAsync(id);
+
+            if (vintie == null)
+            {
+                return NotFound();
+            }
+
+            return vintie;
+        }
+
+        // PUT: api/Vinties/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVintie(int id, Vintie vintie)
+        {
+            if (id != vintie.VintieId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(vintie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VintieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Vinties
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Vintie>> PostVintie(Vintie vintie)
+        {
+            _context.Vinties.Add(vintie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVintie", new { id = vintie.VintieId }, vintie);
+        }
+
+        // DELETE: api/Vinties/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVintie(int id)
+        {
+            var vintie = await _context.Vinties.FindAsync(id);
+            if (vintie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vinties.Remove(vintie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VintieExists(int id)
+        {
+            return _context.Vinties.Any(e => e.VintieId == id);
+        }
+    }
+}
