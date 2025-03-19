b043bb00ff8b9e7ddb89f56c75624ba9afbf626c
mathieu <servonnm@A276-S25>
2025-03-19 15:44:03 +0100

    Manager include

--- SAE401_API_Vinted/Program.cs
+++ SAE401_API_Vinted/Program.cs
@@ -14,6 +14,11 @@ builder.Services.AddDbContext<VintedDBContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("VintedDBContext")));
 
 builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
