f0a9a3e6fd0c2616ec77f73a2f722790e7ebbce9
Anthony Marques Felix <Anthony.Marques-Felix@etu.univ-smb.fr>
2025-03-20 23:09:29 +0100

    AvisController et AvisManager

---
+++ SAE401_API_Vinted/Controllers/AvisController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAE401_API_Vinted.Models.EntityFramework;
+using SAE401_API_Vinted.Models.Repository;
+
+namespace SAE401_API_Vinted.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AvisController : ControllerBase
+    {
+        private readonly IDataRepository<Avis> dataRepository;
+
+        public AvisController(IDataRepository<Avis> dataRepo)
+        {
+            dataRepository = dataRepo;
+        }
+
+        // GET: api/Avis
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Avis>>> GetAvis()
+        {
+            return await dataRepository.GetAllAsync();
+        }
+
+        // GET: api/Avis/5
+        [HttpGet("{id}")]
+        [ActionName("GetById")]
+        public async Task<ActionResult<Avis>> GetAvis(int id)
+        {
+            var Avis = await dataRepository.GetByIdAsync(id);
+
+            if (Avis == null)
+            {
+                return NotFound();
+            }
+
+            return Avis;
+        }
+
+        // PUT: api/Articles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ActionName("Put")]
+        public async Task<IActionResult> Avis(int id, Avis Avis)
+        {
+            if (id != Avis.AvisId)
+            {
+                return BadRequest();
+            }
+
+            var AvisToUpdate = await dataRepository.GetByIdAsync(id);
+            if (AvisToUpdate == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                await dataRepository.PutAsync(AvisToUpdate.Value, Avis);
+                return NoContent();
+            }
+        }
+
+        // POST: api/Avis
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ActionName("Post")]
+        public async Task<ActionResult<Avis>> PostAvis(Avis Avis)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await dataRepository.PostAsync(Avis);
+            return CreatedAtAction("GetbyId", new { id = Avis.AvisId }, Avis); // GetById : nom de l’action
+        }
+
+        // DELETE: api/Avis/5
+        [HttpDelete("{id}")]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteAvis(int id)
+        {
+            var Avis = await dataRepository.GetByIdAsync(id);
+            if (Avis == null)
+            {
+                return NotFound();
+            }
+            await dataRepository.DeleteAsync(Avis.Value);
+            return NoContent();
+        }
+    }
+}
---
+++ SAE401_API_Vinted/Models/DataManager/AvisManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SAE401_API_Vinted.Models.EntityFramework;
+using SAE401_API_Vinted.Models.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SAE401_API_Vinted.Models.DataManager
+{
+    public class AvisManager : IDataRepository<Avis>
+    {
+        readonly VintedDBContext? vintiesDbContext;
+
+        public AvisManager()
+        {
+        }
+
+        public AvisManager(VintedDBContext context)
+        {
+            vintiesDbContext = context;
+        }
+
+        public async Task<ActionResult<IEnumerable<Avis>>> GetAllAsync()
+        {
+            return await vintiesDbContext.Avis.ToListAsync();
+        }
+
+        public async Task<ActionResult<Avis>> GetByIdAsync(int id)
+        {
+            return await vintiesDbContext.Avis.FirstOrDefaultAsync(c => c.AvisId == id);
+        }
+
+        public async Task PostAsync(Avis entity)
+        {
+            await vintiesDbContext.Avis.AddAsync(entity);
+            await vintiesDbContext.SaveChangesAsync();
+        }
+
+        public async Task PutAsync(Avis entityToUpdate, Avis entity)
+        {
+            vintiesDbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.AvisId = entity.AvisId;
+            entityToUpdate.AcheteurId = entity.AcheteurId;
+            entityToUpdate.VendeurId = entity.VendeurId;
+            entityToUpdate.CodeTypeAvis = entity.CodeTypeAvis;
+            entityToUpdate.Commentaire = entity.Commentaire;
+            entityToUpdate.Note = entity.Note;
+            await vintiesDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Avis entity)
+        {
+            vintiesDbContext.Avis.Remove(entity);
+            await vintiesDbContext.SaveChangesAsync();
+        }
+    }
+}
