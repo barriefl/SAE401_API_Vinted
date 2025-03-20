e097a57ba3ef3ca1aabbd4ce0019535848ef3389
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-03-20 08:28:27 +0100

    Codage IGetDataRepository.

---
+++ SAE401_API_Vinted/Models/Repository/IGetDataRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SAE401_API_Vinted.Models.Repository
+{
+    public interface IGetDataRepository<TEntity>
+    {
+        Task<ActionResult<IEnumerable<TEntity>>> GetAllAsync();
+        Task<ActionResult<TEntity>> GetByIdAsync(int id);
+    }
+}
