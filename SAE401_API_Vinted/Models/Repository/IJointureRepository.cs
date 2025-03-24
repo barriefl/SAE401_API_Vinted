879dc81cc9dc13070f2174a113d2b462efb02ca1
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-03-24 09:21:33 +0100

    Ajout d'un Repository : IJointureRepository.

---
+++ SAE401_API_Vinted/Models/Repository/IJointureRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace SAE401_API_Vinted.Models.Repository
+{
+    public interface IJointureRepository<TEntity>
+    {
+        Task PostAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+    }
+}
