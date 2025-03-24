033fcdbe076a302b34f3c6daf14a446a096edb75
YannisUSMB <yannis.mauline@etu.univ-smb.fr>
2025-03-24 10:43:35 +0100

    Controller Image

---
+++ SAE401_API_Vinted/Models/DataManager/ImageManager.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SAE401_API_Vinted.Models.EntityFramework;
+using SAE401_API_Vinted.Models.Repository;
+
+namespace SAE401_API_Vinted.Models.DataManager
+{
+    public class ImageManager : IDataRepository<Image>
+    {
+        readonly VintedDBContext? vintiesDbContext;
+
+        public ImageManager()
+        {
+        }
+
+        public ImageManager(VintedDBContext context)
+        {
+            vintiesDbContext = context;
+        }
+
+        public async Task DeleteAsync(Image entity)
+        {
+            vintiesDbContext.Images.Remove(entity);
+            await vintiesDbContext.SaveChangesAsync();
+        }
+
+        public async Task<ActionResult<IEnumerable<Image>>> GetAllAsync()
+        {
+            return await vintiesDbContext.Images.ToListAsync();
+        }
+
+        public async Task<ActionResult<Image>> GetByIdAsync(int id)
+        {
+            return await vintiesDbContext.Images
+                .Include(c => c.ArticleDeImage)
+                .FirstOrDefaultAsync(c => c.ImageId == id);
+        }
+
+        public async Task PostAsync(Image entity)
+        {
+            await vintiesDbContext.Images.AddAsync(entity);
+            await vintiesDbContext.SaveChangesAsync();
+        }
+
+        public async Task PutAsync(Image entityToUpdate, Image entity)
+        {
+            vintiesDbContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.ImageId = entity.ImageId;
+            entityToUpdate.ArticleId = entity.ArticleId;
+            entityToUpdate.Url = entity.Url;
+            await vintiesDbContext.SaveChangesAsync();
+        }
+    }
+}
