1b31541e498a3d9bfaffeb0bd40f49c0b4fe6828
hugoclercrenaud <Hugo.Clerc-Renaud@etu.univ-smb.fr>
2025-03-25 17:38:15 +0100

    Methode categorie

--- SAE401_API_Vinted/Models/Repository/ICategorieRepository.cs
+++ SAE401_API_Vinted/Models/Repository/ICategorieRepository.cs
@@ -1,9 +1,11 @@
-﻿using SAE401_API_Vinted.Models.EntityFramework;
+﻿using Microsoft.AspNetCore.Mvc;
+using SAE401_API_Vinted.Models.EntityFramework;
 
 namespace SAE401_API_Vinted.Models.Repository
 {
     public interface ICategorieRepository : IGetDataRepository<Categorie>
     {
-        Task<IEnumerable<Categorie>> GetSousCategories(int idParent);
+        Task<ActionResult<IEnumerable<Categorie>>> GetSousCategories(int idParent);
+
     }
 }
