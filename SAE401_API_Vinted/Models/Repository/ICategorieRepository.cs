c50ce703013c0ff6234789580b33d6686f89bed2
YannisUSMB <yannis.mauline@etu.univ-smb.fr>
2025-03-25 17:34:40 +0100

    ajout get by parent pour le controller categorie

---
+++ SAE401_API_Vinted/Models/Repository/ICategorieRepository.cs
@@ -0,0 +1,9 @@
+﻿using SAE401_API_Vinted.Models.EntityFramework;
+
+namespace SAE401_API_Vinted.Models.Repository
+{
+    public interface ICategorieRepository : IGetDataRepository<Categorie>
+    {
+        Task<IEnumerable<Categorie>> GetSousCategories(int idParent);
+    }
+}
