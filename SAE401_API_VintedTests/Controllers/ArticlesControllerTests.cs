24fc9839ebad7130a0ecb27fb9449a7080004a79
YannisUSMB <yannis.mauline@etu.univ-smb.fr>
2025-03-26 10:36:45 +0100

    Finission tests ArticleController

--- SAE401_API_VintedTests/Controllers/ArticlesControllerTests.cs
+++ SAE401_API_VintedTests/Controllers/ArticlesControllerTests.cs
@@ -634,5 +634,431 @@ namespace SAE401_API_Vinted.Controllers.Tests
             // Assert
             Assert.IsInstanceOfType(actionResult, typeof(NoContentResult), "Pas un NoContentResult"); // Test du type de retour
         }
+
+
+        //
+        // TESTS DES GET DE COULEURS
+        //
+
+        // TESTS UNITAIRES
+
+        [TestMethod()]
+        public void GetCouleursTest()
+        {
+            //Arrange
+            var lesCouleurs = context.Couleurs.ToList();
+
+            //Act
+            var result = controller.GetCouleurs().Result;
+
+            //Assert
+            Assert.IsNotNull(result, "Aucune couleurs retournées");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<IEnumerable<Couleur>>), "Result n'est pas un action result");
+            CollectionAssert.AreEqual(result.Value.ToList(), lesCouleurs, "Les listes des couleurs ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetCouleurByIdTest_ExistingId()
+        {
+            //Arrange
+            Couleur couleur = context.Couleurs.Where(a => a.CouleurId == 1).FirstOrDefault();
+
+            //Act
+            var result = controller.GetCouleur(1).Result;
+
+            //Assert
+            Assert.IsNotNull(result, "Couleur non retournée");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Couleur>), "Result n'est pas un action result");
+            Assert.AreEqual(result.Value, couleur, "Les couleurs ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetCouleurByIdTest_UnkownId()
+        {
+            //Arrange
+
+            //Act
+            var result = controller.GetCouleur(4273).Result;
+
+            //Assert
+            Assert.AreEqual(((NotFoundResult)result.Result).StatusCode, StatusCodes.Status404NotFound, "Result ne retourne pas 404 not found");
+        }
+
+        // TESTS MOCKS
+
+        [TestMethod()]
+        public void GetCouleurById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            // Arrange
+            Couleur couleur = new Couleur()
+            {
+                CouleurId = 1,
+                Libelle = "Noir"
+            };
+            mockArticleRepository.Setup(x => x.GetCouleurByIdAsync(1).Result).Returns(couleur);
+
+            // Act
+            var result = mockArticleController.GetCouleur(1).Result;
+
+            // Assert
+            Assert.IsNotNull(result, "Aucun résultat.");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Couleur>), "Pas un ActionResult.");
+            Assert.IsNull(result.Result, "Il y a une erreur.");
+            Assert.IsInstanceOfType(result.Value, typeof(Couleur), "Pas une Couleur");
+            Assert.AreEqual(couleur, result.Value, "Couleurs pas identiques.");
+        }
+
+        [TestMethod]
+        public void GetCouleurById_UnknownIdPassed_ReturnsNotFoundResult_Moq()
+        {
+            // Arrange
+
+            // Act
+            var actionResult = mockArticleController.GetCouleur(0).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+
+        //
+        // TESTS DES GET DE TAILLE
+        //
+
+        // TESTS UNITAIRES
+
+        [TestMethod()]
+        public void GetTaillesTest()
+        {
+            //Arrange
+            var lesTailles = context.Tailles.ToList();
+
+            //Act
+            var result = controller.GetTailles().Result;
+
+            //Assert
+            Assert.IsNotNull(result, "Aucune taille retournée");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<IEnumerable<Taille>>), "Result n'est pas un action result");
+            CollectionAssert.AreEqual(result.Value.ToList(), lesTailles, "Les listes des tailles ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetTailleByIdTest_ExistingId()
+        {
+            //Arrange
+            Taille taille = context.Tailles.Where(a => a.TailleId == 1).FirstOrDefault();
+
+            //Act
+            var result = controller.GetTaille(1).Result;
+
+            //Assert
+            Assert.IsNotNull(result, "Taille non retournée");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Taille>), "Result n'est pas un action result");
+            Assert.AreEqual(result.Value, taille, "Les tailles ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetTailleByIdTest_UnkownId()
+        {
+            //Arrange
+
+            //Act
+            var result = controller.GetTaille(4273).Result;
+
+            //Assert
+            Assert.AreEqual(((NotFoundResult)result.Result).StatusCode, StatusCodes.Status404NotFound, "Result ne retourne pas 404 not found");
+        }
+
+        // TESTS MOCKS
+
+        [TestMethod()]
+        public void GetTailleById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            // Arrange
+            Taille taille = new Taille()
+            {
+                TailleId = 1,
+                Libelle = "Noir"
+            };
+            mockArticleRepository.Setup(x => x.GetTailleByIdAsync(1).Result).Returns(taille);
+
+            // Act
+            var result = mockArticleController.GetTaille(1).Result;
+
+            // Assert
+            Assert.IsNotNull(result, "Aucun résultat.");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Taille>), "Pas un ActionResult.");
+            Assert.IsNull(result.Result, "Il y a une erreur.");
+            Assert.IsInstanceOfType(result.Value, typeof(Taille), "Pas une Taille");
+            Assert.AreEqual(taille, result.Value, "Tailles pas identiques.");
+        }
+
+        [TestMethod]
+        public void GetTailleById_UnknownIdPassed_ReturnsNotFoundResult_Moq()
+        {
+            // Arrange
+
+            // Act
+            var actionResult = mockArticleController.GetTaille(0).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+
+        //
+        // TESTS DES GET DE MARQUE
+        //
+
+        // TESTS UNITAIRES
+
+        [TestMethod()]
+        public void GetMarquesTest()
+        {
+            //Arrange
+            var lesMarques = context.Marques.ToList();
+
+            //Act
+            var result = controller.GetMarques().Result;
+
+            //Assert
+            Assert.IsNotNull(result, "Aucune marque retournée");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<IEnumerable<Marque>>), "Result n'est pas un action result");
+            CollectionAssert.AreEqual(result.Value.ToList(), lesMarques, "Les listes des marques ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetMarqueByIdTest_ExistingId()
+        {
+            //Arrange
+            Marque marque = context.Marques.Where(a => a.MarqueId == 1).FirstOrDefault();
+
+            //Act
+            var result = controller.GetMarque(1).Result;
+
+            //Assert
+            Assert.IsNotNull(result, "Marque non retournée");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Marque>), "Result n'est pas un action result");
+            Assert.AreEqual(result.Value, marque, "Les marques ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetMarqueByIdTest_UnkownId()
+        {
+            //Arrange
+
+            //Act
+            var result = controller.GetMarque(4273).Result;
+
+            //Assert
+            Assert.AreEqual(((NotFoundResult)result.Result).StatusCode, StatusCodes.Status404NotFound, "Result ne retourne pas 404 not found");
+        }
+
+        // TESTS MOCKS
+
+        [TestMethod()]
+        public void GetMarqueById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            // Arrange
+            Marque marque = new Marque()
+            {
+                MarqueId = 1,
+                Libelle = "Durex"
+            };
+            mockArticleRepository.Setup(x => x.GetMarqueByIdAsync(1).Result).Returns(marque);
+
+            // Act
+            var result = mockArticleController.GetMarque(1).Result;
+
+            // Assert
+            Assert.IsNotNull(result, "Aucun résultat.");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Marque>), "Pas un ActionResult.");
+            Assert.IsNull(result.Result, "Il y a une erreur.");
+            Assert.IsInstanceOfType(result.Value, typeof(Marque), "Pas une Marque");
+            Assert.AreEqual(marque, result.Value, "Marques pas identiques.");
+        }
+
+        [TestMethod]
+        public void GetMarqueById_UnknownIdPassed_ReturnsNotFoundResult_Moq()
+        {
+            // Arrange
+
+            // Act
+            var actionResult = mockArticleController.GetMarque(0).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+
+        //
+        // TESTS DES GET DE ETATARTICLE
+        //
+
+        // TESTS UNITAIRES
+
+        [TestMethod()]
+        public void GetEtatsArticlesTest()
+        {
+            //Arrange
+            var lesEtatArticles = context.EtatsArticles.ToList();
+
+            //Act
+            var result = controller.GetEtatsArticles().Result;
+
+            //Assert
+            Assert.IsNotNull(result, "Aucun EtatArticle retourné");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<IEnumerable<EtatArticle>>), "Result n'est pas un action result");
+            CollectionAssert.AreEqual(result.Value.ToList(), lesEtatArticles, "Les listes des EtatArticle ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetEtatArticleByIdTest_ExistingId()
+        {
+            //Arrange
+            EtatArticle etatArticle = context.EtatsArticles.Where(a => a.EtatArticleId == 1).FirstOrDefault();
+
+            //Act
+            var result = controller.GetEtatArticle(1).Result;
+
+            //Assert
+            Assert.IsNotNull(result, "EtatArticle non retournée");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<EtatArticle>), "Result n'est pas un action result");
+            Assert.AreEqual(result.Value, etatArticle, "Les EtatArticles ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetEtatArticleByIdTest_UnkownId()
+        {
+            //Arrange
+
+            //Act
+            var result = controller.GetEtatArticle(4273).Result;
+
+            //Assert
+            Assert.AreEqual(((NotFoundResult)result.Result).StatusCode, StatusCodes.Status404NotFound, "Result ne retourne pas 404 not found");
+        }
+
+        // TESTS MOCKS
+
+        [TestMethod()]
+        public void GetEtatArticleById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            // Arrange
+            EtatArticle etatArticle = new EtatArticle()
+            {
+                EtatArticleId = 1,
+                Libelle = "Neuf avec etiquette",
+                Description = "Article neuf, jamais porte/utilise avec etiquettes ou dans son emballage d origine."
+            };
+            mockArticleRepository.Setup(x => x.GetEtatArticleByIdAsync(1).Result).Returns(etatArticle);
+
+            // Act
+            var result = mockArticleController.GetEtatArticle(1).Result;
+
+            // Assert
+            Assert.IsNotNull(result, "Aucun résultat.");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<EtatArticle>), "Pas un ActionResult.");
+            Assert.IsNull(result.Result, "Il y a une erreur.");
+            Assert.IsInstanceOfType(result.Value, typeof(EtatArticle), "Pas un EtatArticle");
+            Assert.AreEqual(etatArticle, result.Value, "Les EtatArticle pas identiques.");
+        }
+
+        [TestMethod]
+        public void GetEtatArticleById_UnknownIdPassed_ReturnsNotFoundResult_Moq()
+        {
+            // Arrange
+
+            // Act
+            var actionResult = mockArticleController.GetEtatArticle(0).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
+
+
+        //
+        // TESTS DES GET DE MARQUE
+        //
+
+        // TESTS UNITAIRES
+
+        [TestMethod()]
+        public void GetEtatsVenteTest()
+        {
+            //Arrange
+            var lesEtatsVente = context.EtatsVentesArticles.ToList();
+
+            //Act
+            var result = controller.GetEtatsVentesArticles().Result;
+
+            //Assert
+            Assert.IsNotNull(result, "Aucun etatVente retournée");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<IEnumerable<EtatVente>>), "Result n'est pas un action result");
+            CollectionAssert.AreEqual(result.Value.ToList(), lesEtatsVente, "Les listes des EtatsVente ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetEtatVenteByIdTest_ExistingId()
+        {
+            //Arrange
+            EtatVente etatVente = context.EtatsVentesArticles.Where(a => a.EtatVenteArticleId == 1).FirstOrDefault();
+
+            //Act
+            var result = controller.GetEtatVenteArticle(1).Result;
+
+            //Assert
+            Assert.IsNotNull(result, "EtatVente non retournée");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<EtatVente>), "Result n'est pas un action result");
+            Assert.AreEqual(result.Value, etatVente, "Les EtatsVente ne sont pas égales");
+        }
+
+        [TestMethod()]
+        public void GetEtatVenteByIdTest_UnkownId()
+        {
+            //Arrange
+
+            //Act
+            var result = controller.GetEtatVenteArticle(4273).Result;
+
+            //Assert
+            Assert.AreEqual(((NotFoundResult)result.Result).StatusCode, StatusCodes.Status404NotFound, "Result ne retourne pas 404 not found");
+        }
+
+        // TESTS MOCKS
+
+        [TestMethod()]
+        public void GetEtatVenteById_ExistingIdPassed_ReturnsRightItem_AvecMoq()
+        {
+            // Arrange
+            EtatVente etatVente = new EtatVente()
+            {
+                EtatVenteArticleId = 1,
+                Libelle = "En ligne"
+            };
+            mockArticleRepository.Setup(x => x.GetEtatVenteByIdAsync(1).Result).Returns(etatVente);
+
+            // Act
+            var result = mockArticleController.GetEtatVenteArticle(1).Result;
+
+            // Assert
+            Assert.IsNotNull(result, "Aucun résultat.");
+            Assert.IsInstanceOfType(result, typeof(ActionResult<EtatVente>), "Pas un ActionResult.");
+            Assert.IsNull(result.Result, "Il y a un erreur.");
+            Assert.IsInstanceOfType(result.Value, typeof(EtatVente), "Pas un EtatVente");
+            Assert.AreEqual(etatVente, result.Value, "EtatVente pas identiques.");
+        }
+
+        [TestMethod]
+        public void GetEtatVenteById_UnknownIdPassed_ReturnsNotFoundResult_Moq()
+        {
+            // Arrange
+
+            // Act
+            var actionResult = mockArticleController.GetEtatVenteArticle(0).Result;
+
+            // Assert
+            Assert.IsInstanceOfType(actionResult.Result, typeof(NotFoundResult));
+        }
     }
 }
