eb3eaa2d4055ce59ffb2d823a07e2d5e5a90d6d4
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-03-27 09:23:00 +0100

    Documentation TypesTailleController.

--- SAE401_API_Vinted/Controllers/TypesTailleController.cs
+++ SAE401_API_Vinted/Controllers/TypesTailleController.cs
@@ -16,23 +16,46 @@ namespace SAE401_API_Vinted.Controllers
     [ApiController]
     public class TypesTailleController : ControllerBase
     {
-
         private readonly IGetDataRepository<TypeTaille> dataRepositoryTypeTaille;
 
+        /// <summary>
+        /// Constructeur pour le contrôleur TypesTailleController.
+        /// </summary>
+        /// <param name="dataRepo">Le DataRepository utilisé pour accéder aux types de taille.</param>
         public TypesTailleController(IGetDataRepository<TypeTaille> dataRepo)
         {
             dataRepositoryTypeTaille = dataRepo;
         }
 
+        /// <summary>
+        /// Récupère toutes les types de taille.
+        /// </summary>
+        /// <returns>Une liste de types de taille sous forme de réponse HTTP 200 OK.</returns>
+        /// <response code="200">La liste des types de taille a été récupérée avec succès.</response>
+        /// <response code="500">Une erreur interne s'est produite sur le serveur.</response>
+        // GET: api/TypesTaille
         [HttpGet]
-        [ActionName("GetAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<IEnumerable<TypeTaille>>> GeTypesTaille()
         {
             return await dataRepositoryTypeTaille.GetAllAsync();
         }
 
+        /// <summary>
+        /// Récupère un type de taille.
+        /// </summary>
+        /// <param name="id">L'id du type de taille.</param>
+        /// <returns>Un type de taille sous forme de réponse HTTP 200 OK.</returns>
+        /// <response code="200">Le type de taille a été récupéré avec succès.</response>
+        /// <response code="404">Le type de taille demandé n'existe pas.</response>
+        /// <response code="500">Une erreur interne s'est produite sur le serveur.</response>
+        // GET: api/TypesTaille/GetById/5
         [HttpGet("{id}")]
-        [ActionName("GetbyId")]
+        [ActionName("GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<TypeTaille>> GetTypeTaille(int id)
         {
             var typeTaille = await dataRepositoryTypeTaille.GetByIdAsync(id);
@@ -48,6 +71,5 @@ namespace SAE401_API_Vinted.Controllers
 
             return typeTaille;
         }
-
     }
 }
