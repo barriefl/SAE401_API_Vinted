53e3f46ac91b9161357729792fcfe11182a1c336
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-03-27 08:43:40 +0100

    Documentation MatiereArticlesController.

--- SAE401_API_Vinted/Controllers/MatiereArticlesController.cs
+++ SAE401_API_Vinted/Controllers/MatiereArticlesController.cs
@@ -16,14 +16,30 @@ namespace SAE401_API_Vinted.Controllers
     {
         private readonly IJointureRepository<MatiereArticle> dataRepository;
 
+        /// <summary>
+        /// Constructeur pour le contrôleur MatiereArticlesController.
+        /// </summary>
+        /// <param name="dataRepo">Le DataRepository utilisé pour accéder aux données de la table de jointure MatiereArticle.</param>
         public MatiereArticlesController(IJointureRepository<MatiereArticle> dataRepo)
         {
             dataRepository = dataRepo;
         }
 
+        /// <summary>
+        /// Récupère un MatiereArticle.
+        /// </summary>
+        /// <param name="matiereId">L'id de la matière.</param>
+        /// <param name="articleId">L'id de l'article.</param>
+        /// <returns>Un MatiereArticle sous forme de réponse HTTP 200 OK.</returns>
+        /// <response code="200">Le MatiereArticle a été récupéré avec succès.</response>
+        /// <response code="404">Le MatiereArticle demandé n'existe pas.</response>
+        /// <response code="500">Une erreur interne s'est produite sur le serveur.</response>
         // GET: api/MatiereArticles/GetByIds/5&5
         [HttpGet("{matiereId}&{articleId}")]
         [ActionName("GetByIds")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<MatiereArticle>> GetMatiereArticle(int matiereId, int articleId)
         {
             var matiereArticle = await dataRepository.GetByIdsAsync(matiereId, articleId);
@@ -36,10 +52,21 @@ namespace SAE401_API_Vinted.Controllers
             return matiereArticle;
         }
 
+        /// <summary>
+        /// Créer un MatiereArticle.
+        /// </summary>
+        /// <param name="favoris">L'objet MatiereArticle.</param>
+        /// <returns>Une réponse HTTP 201 Created.</returns>
+        /// <response code="201">Le MatiereArticle a été créé avec succès.</response>
+        /// <response code="400">Le format du MatiereArticle est incorrect.</response>
+        /// <response code="500">Une erreur interne s'est produite sur le serveur.</response>
         // POST: api/MatiereArticles/Post
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         [ActionName("Post")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<MatiereArticle>> PostMatiereArticle(MatiereArticle matiereArticle)
         {
             if (!ModelState.IsValid)
@@ -50,9 +77,21 @@ namespace SAE401_API_Vinted.Controllers
             return CreatedAtAction("GetByIds", new { matiereId = matiereArticle.MatiereId, articleId = matiereArticle.ArticleId }, matiereArticle);
         }
 
+        /// <summary>
+        /// Supprime un MatiereArticle.
+        /// </summary>
+        /// <param name="matiereId">L'id de la matière.</param>
+        /// <param name="articleId">L'id de l'article.</param>
+        /// <returns>Une réponse HTTP 204 No Content.</returns>
+        /// <response code="204">Le MatiereArticle a été supprimé avec succès.</response>
+        /// <response code="404">Le MatiereArticle n'existe pas.</response>
+        /// <response code="500">Une erreur interne s'est produite sur le serveur.</response>
         // DELETE: api/MatiereArticles/Delete/5&5
         [HttpDelete("{matiereId}&{articleId}")]
         [ActionName("Delete")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteMatiereArticle(int matiereId, int articleId)
         {
             var matiereArticle = await dataRepository.GetByIdsAsync(matiereId, articleId);
